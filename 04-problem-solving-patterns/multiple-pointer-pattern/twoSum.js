2c8d6af71984d20c9e3219eb340035c3551c317b
tanvirch0750 <tanvirch7575@gmail.com>
2024-11-28 21:19:17 +0600

    add two some

---
+++ 04-problem-solving-patterns/multiple-pointer-pattern/twoSum.js
@@ -0,0 +1,17 @@
+const twoSum = function(numbers, target) {
+    let left = 0;
+    let right = numbers.length - 1;
+
+    while (left < right) {
+        let sum = numbers[left] + numbers[right];
+        if (sum === target) {
+            return [left + 1, right + 1];
+        } else if (sum < target) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+};
+
+console.log(twoSum([2,7,11,15], 9));
